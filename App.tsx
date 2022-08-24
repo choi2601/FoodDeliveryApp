37ba6d0f6b011498e7103ead09e7c71dc937985c
choi2601 <byeonghyeonchoe55@gmail.com>
2022-08-24 21:15:19 +0900

    모바일 position 원리 실습

--- App.tsx
+++ App.tsx
@@ -1,10 +1,18 @@
-import * as React from 'react';
+import React, {useState} from 'react';
 import {NavigationContainer, ParamListBase} from '@react-navigation/native';
 import {
   createNativeStackNavigator,
   NativeStackScreenProps,
 } from '@react-navigation/native-stack';
-import {Text, TouchableHighlight, View, LogBox, Pressable} from 'react-native';
+import {
+  Text,
+  TouchableHighlight,
+  View,
+  LogBox,
+  Pressable,
+  StyleSheet,
+  Dimensions,
+} from 'react-native';
 import {useCallback} from 'react';
 
 type RootStackParamList = {
@@ -15,6 +23,8 @@ type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
 type DetailsScreenProps = NativeStackScreenProps<ParamListBase, 'Details'>;
 
 function HomeScreen({navigation, route}: HomeScreenProps) {
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+
   const onClick = useCallback(() => {
     navigation.navigate('Details');
   }, [navigation]);
@@ -37,6 +47,29 @@ function HomeScreen({navigation, route}: HomeScreenProps) {
       <View style={{flex: 2, backgroundColor: 'orange'}}>
         <Text>Seconds</Text>
       </View>
+      <Pressable
+        style={{position: 'absolute', ...StyleSheet.absoluteFillObject}}
+        onPress={() => setIsVisible(true)}>
+        <Text>모달 켜기</Text>
+      </Pressable>
+      {isVisible && (
+        <Pressable onPress={() => setIsVisible(false)} style={styles.modal}>
+          <View style={styles.modalInner}>
+            <Text
+              style={{flex: 5, position: 'relative', backgroundColor: 'blue'}}>
+              Hello
+            </Text>
+            <View style={{flexDirection: 'row', marginTop: 20}}>
+              <Pressable style={{flex: 1, alignItems: 'center'}}>
+                <Text>네</Text>
+              </Pressable>
+              <Pressable style={{flex: 1, alignItems: 'center'}}>
+                <Text>아니요</Text>
+              </Pressable>
+            </View>
+          </View>
+        </Pressable>
+      )}
     </>
   );
 }
@@ -90,5 +123,36 @@ function App() {
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  modal: {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    // position: 'absolute',
+    // left: 0,
+    // right: 0,
+    // top: 0,
+    // bottom: 0,
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'center',
+  },
+  modalInner: {
+    position: 'absolute',
+    backgroundColor: 'orange',
+    top: 50,
+    bottom: 50,
+    left: 50,
+    right: 50,
+    width: Dimensions.get('window').width - 100,
+    height: 300,
+    marginVertical: 50,
+    borderRadius: 20,
+    padding: 20,
+    shadowColor: 'black',
+    shadowOpacity: 0.5,
+    shadowRadius: 5,
+    shadowOffset: {width: 5, height: 5},
+    elevation: 15, // 안드로이드 그림자 효과
+  },
+});
 
 export default App;
