9ee280924d28a585f5d67d774577f731a4ea8a6c
choi2601 <byeonghyeonchoe55@gmail.com>
2022-08-27 19:03:19 +0900

    앱 종료 시에도 로그인 유지 기능 구현

--- App.tsx
+++ App.tsx
@@ -1,16 +1,21 @@
 import React, {useEffect, useState} from 'react';
+import {Platform, Alert} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import axios, {AxiosError} from 'axios';
+import Config from 'react-native-config';
 import {Provider, useSelector} from 'react-redux';
+import EncryptedStorage from 'react-native-encrypted-storage';
 import Settings from './src/pages/Settings';
 import Orders from './src/pages/Orders';
 import Delivery from './src/pages/Delivery';
 import SignIn from './src/pages/SignIn';
 import SignUp from './src/pages/SignUp';
-import store from './src/store';
+import store, {useAppDispatch} from './src/store';
 import {RootState} from './src/store/reducer';
 import useSocket from './src/hooks/useSocket';
+import userSlice from './src/slices/user';
 
 export type LoggedInParamList = {
   Orders: undefined;
@@ -28,21 +33,22 @@ const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 function AppInner() {
+  const dispatch = useAppDispatch();
+
   const isLoggedIn = useSelector((state: RootState) => !!state.user.email);
   const [socket, disconnect] = useSocket();
 
   useEffect(() => {
-    const helloCallback = (data: any) => {
+    const callback = (data: any) => {
       console.log(data);
     };
     if (socket && isLoggedIn) {
-      console.log(socket);
-      socket.emit('login', 'hello');
-      socket.on('hello', helloCallback);
+      socket.emit('acceptOrder', 'hello');
+      socket.on('order', callback);
     }
     return () => {
       if (socket) {
-        socket.off('hello', helloCallback);
+        socket.off('order', callback);
       }
     };
   }, [isLoggedIn, socket]);
@@ -52,6 +58,44 @@ function AppInner() {
       disconnect();
     }
   }, [isLoggedIn, disconnect]);
+
+  useEffect(() => {
+    const getTokenAndRefresh = async () => {
+      try {
+        const token = await EncryptedStorage.getItem('refreshToken');
+        if (!token) {
+          return;
+        }
+        const response = await axios.post(
+          `${
+            Platform.OS === 'android'
+              ? Config.API_ANDROID_URL
+              : Config.API_IOS_URL
+          }/refreshToken`,
+          {},
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          },
+        );
+        dispatch(
+          userSlice.actions.setUser({
+            name: response.data.data.name,
+            email: response.data.data.email,
+            accessToken: response.data.data.accessToken,
+          }),
+        );
+      } catch (error) {
+        if ((error as AxiosError).response?.data.code === 'expired') {
+          Alert.alert('알림', '다시 로그인 해주세요.');
+        }
+      } finally {
+        // TODO: splash screen active off
+      }
+    };
+    getTokenAndRefresh();
+  }, [dispatch]);
 
   return (
     <NavigationContainer>
